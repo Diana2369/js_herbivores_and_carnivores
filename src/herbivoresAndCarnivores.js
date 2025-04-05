5b6e53c254d8cd8df88d7f022a995df7a6108cbe
Diana <dinamitka2369@gmail.com>
2025-04-05 11:47:49 +0200

    result

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,45 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    Animal.alive.push(this);
+  }
+
+  die() {
+    const index = Animal.alive.indexOf(this);
+
+    if (index !== -1) {
+      Animal.alive.splice(index, 1);
+    }
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(otherAnimal) {
+    if (otherAnimal instanceof Herbivore && !otherAnimal.hidden) {
+      otherAnimal.health -= 50;
+
+      if (otherAnimal.health <= 0) {
+        // Удаляем животное из Animal.alive
+        Animal.alive = Animal.alive.filter((animal) => animal !== otherAnimal);
+      }
+    }
+  }
 }
 
 module.exports = {
